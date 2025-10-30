0170e52e626277580a703c70d4be6676c7f792b6
Ozan6707 <105652367+Ozan6707@users.noreply.github.com>
2025-10-31 00:56:35 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-10-30T21:56:28.454Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2025-10-30T21:56:35.200Z
+Commit: Fix memory leak in event handlers
