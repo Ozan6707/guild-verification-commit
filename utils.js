f31491f5dd64a48c334d4758af1e78b7640907fc
Ozan6707 <105652367+Ozan6707@users.noreply.github.com>
2025-10-31 00:56:29 +0300

    Fix CSS styling bug on mobile devices

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-10-30T21:56:22.716Z
-Commit: Fix database connection timeout
+Timestamp: 2025-10-30T21:56:28.454Z
+Commit: Fix CSS styling bug on mobile devices
