e83d18fade89d4d8e115f5cc49a9d70b3ac6e482
Ozan6707 <105652367+Ozan6707@users.noreply.github.com>
2026-06-09 01:35:33 +0300

    Patch security vulnerability in authentication

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-06-08T22:35:11.439Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-06-08T22:35:32.466Z
+Commit: Patch security vulnerability in authentication
