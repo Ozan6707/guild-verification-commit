5875dc245447e0aa6c6bd0a865e165b33905bd50
Ozan6707 <105652367+Ozan6707@users.noreply.github.com>
2026-06-09 01:37:11 +0300

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-06-08T22:37:06.879Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-06-08T22:37:10.401Z
+Commit: Fix memory leak in event handlers
