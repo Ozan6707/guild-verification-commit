7eb523a6cbc6f12f018cda81243ed8f4636e1f3f
Ozan6707 <105652367+Ozan6707@users.noreply.github.com>
2026-06-09 01:37:39 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-06-08T22:37:36.587Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-06-08T22:37:38.128Z
+Commit: Fix memory leak in event handlers
